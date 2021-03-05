be510a5949df4a0ac131f6296297ffff0ca13db2
98129182 <60801833+98129182@users.noreply.github.com>
2021-03-05 14:51:38 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import flask
+from flask import Flask, render_template, redirect
+app = Flask('Project Day website')
+#Redirect if no route specified
+@app.route('/')
+def redirect_to_home():
+  print("Pong!")
+  return redirect('https://HK-Campaign-Air-Pollution-and-Shark-Finning.98129182.repl.co/home',302)
+@app.route('/home')
+def render_home():
+  print("Rendering....")
+  return(render_template('home.html'))
+@app.route('/shark_finning')
+def render_shark_finning():
+  return(render_template('shark_finning.html'))
+@app.route('/air_pollution')
+def render_air_pollution():
+  return(render_template('air_pollution.html'))
+app.run(host='0.0.0.0',port=8080)
