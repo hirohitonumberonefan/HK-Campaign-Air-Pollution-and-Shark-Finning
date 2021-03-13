cca9667de1891978eee47dc558c9b7822de314e3
98129182 <minecraftgamer00100@gmail.com>
2021-03-13 09:20:44 +0000

    working on converting to production server

---
+++ HK-Campaign-Air-Pollution-and-Shark-Finning/HK-Campaign-Air-Pollution-and-Shark-Finning/__init__.py
@@ -0,0 +1,6 @@
+import flask
+import time
+import asyncio
+import flask_script
+import os
+import views
---
+++ HK-Campaign-Air-Pollution-and-Shark-Finning/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+from setuptools import setup
+setup(
+  name = 'HK-Campaign-Air-Pollution-and-Shark-Finning',
+  packages = ['flask','aiohttp','os','time','asyncio',],
+  install_requires = ['flask','aiohttp','os','time','asyncio',],
+)
