526435f39f4dafb05f916fa63d9a6c15386c025e
98129182 <minecraftgamer00100@gmail.com>
2021-03-13 13:04:09 +0000

    reverted previous commit, return 404 when on 404 page

--- HK-Campaign-Air-Pollution-and-Shark-Finning/HK-Campaign-Air-Pollution-and-Shark-Finning/__init__.py
+++
@@ -1,6 +0,0 @@
-import flask
-import time
-import asyncio
-import flask_script
-import os
-import views
--- HK-Campaign-Air-Pollution-and-Shark-Finning/setup.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-from setuptools import setup
-setup(
-  name = 'HK-Campaign-Air-Pollution-and-Shark-Finning',
-  packages = ['flask','aiohttp','os','time','asyncio',],
-  install_requires = ['flask','aiohttp','os','time','asyncio',],
-)
